fef6561e15b2c11656e131d4bd86ca700cd64d78
Kamila Kiedrzynek <kamilakiedrzynek@gmail.com>
2020-10-21 18:31:15 +0200

    Input component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Input from './components/Input';
 
 function App() {
   return (
     <div className="App">
+      <Input placeholder="Type here the tempo"></Input>
     </div>
   );
 }
--- src/func.js
+++ src/func.js
@@ -2,3 +2,5 @@ const metronom = (bpm) => {
     let tempo = bpm / 60;
     return tempo;
 }
+
+export default metronom;
